c07641bde4cf6df163e10dcb9271fb54562f8ec1
Felipe Genuino <design@felipegenuino.com>
2022-10-05 22:10:39 -0300

    config cor e fonte

--- 02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -15,20 +15,21 @@ const buttonVariants = {
 };
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-  background-color: ${(props) => props.theme.colors.blueDark};
-  color: ${(props) => props.theme.colors.text};
+  background-color: ${(props) => props.theme.colors["green-500"]};
+  color: ${(props) => props.theme.colors.white};
 
   width: 100px;
   height: 50px;
-  border: 1px solid #ccc;
+  border: 1px solid transparent;
   border-radius: 4px;
   cursor: pointer;
   font-size: 1rem;
   font-weight: 600;
   padding: 0.5rem 1rem;
   transition: background-color 0.2s;
+  margin: 8px;
 
   &:hover {
-    background-color: #e0e0e0;
+    background-color: ${(props) => props.theme.colors["green-700"]};
   }
 `;
---
+++ 02-ignite-timer/src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus{
+        outline: 1;
+        box-shadow: 0 0 0 2px ${(props) => props.theme.colors["green-500"]};
+    }
+    html {
+        font-size: 62.5%;
+    }
+    body {
+        background-color: ${(props) => props.theme.colors["gray-900"]};
+        color: ${(props) => props.theme.colors["gray-300"]};
+        font-family: ${(props) => props.theme.fonts.heading}, sans-serif;
+        font-weight:  ${(props) => props.theme.fonts.weight.medium};
+        font-size: 1.6rem;
+    }
+    button {
+        cursor: pointer;
+    }
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    ul {
+        list-style: none;
+    }
+    input, button, textarea {
+        font: 1.6rem "Roboto", sans-serif;
+    }
+    @media (max-width: 1080px) {
+        html {
+            font-size: 93.75%;
+        }
+    }
+    @media (max-width: 720px) {
+        html {
+            font-size: 87.5%;
+        }
+    }
+`;
